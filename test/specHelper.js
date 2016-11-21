95fb6a4a36f51aecb24d6a70c53f8731f52deae6
Hans Thorhauge Dam <hans.dam@gmail.com>
2016-11-21 11:35:21 -0500

    Added tests for correct keydown behaviour in various contexts

---
+++ test/specHelper.js
@@ -0,0 +1,35 @@
+var Key = {
+    escape: 27,
+    up: 38,
+    down: 40,
+    pageUp: 33,
+    pageDown: 34,
+    enter: 13,
+    tab: 9,
+    t: 84,
+    'delete': 46,
+    left: 37,
+    right: 39,
+    shift: 16,
+    control: 17
+};
+
+function keyDown(element, options) {
+    var defaultOptions = {
+        shiftKey: false,
+        ctrlKey: false
+    };
+    options = $.extend(defaultOptions, options);
+    var e = $.Event("keydown", options);
+    element.trigger(e);
+}
+
+function keyUp(element, options) {
+    var defaultOptions = {
+        shiftKey: false,
+        ctrlKey: false
+    };
+    options = $.extend(defaultOptions, options);
+    var e = $.Event("keyup", options);
+    element.trigger(e);
+}
